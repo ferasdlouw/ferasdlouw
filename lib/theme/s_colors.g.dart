0d1f920e551016cbeebf7ebd6d24b7beab5a879f
Parabeac-Cloud <admin@parabeac.com>
2023-08-18 07:32:13 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/s_colors.g.dart
@@ -0,0 +1,26 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class SColors {
+
+static const Color white = Color(0xffffffff);
+
+static const Color green = Color(0xff7bed8d);
+
+static const Color mediumGrey = Color(0xffa6bcd0);
+
+static const Color mediumGrey2 = Color(0xff748a9d);
+
+static const Color lighterGrey = Color(0xfff0f4f8);
+
+static const Color lightGrey = Color(0xffdbe2ed);
+
+static const Color darkerGrey = Color(0xff404e5a);
+
+static const Color darkGrey = Color(0xff4e5d6a);
+}
---
+++ lib/theme/s_text_styles.g.dart
@@ -0,0 +1,100 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class STextStyles {
+
+static const TextStyle acuminProSemiCondensed(Medium)—16px = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Medium)—16px2 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Medium)—16px3 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Regular)—24px = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Regular)—20px = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Bold)—14px = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Bold)—14px2 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Regular)—30px = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle acuminProSemiCondensed(Medium)—14px = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
